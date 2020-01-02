c2a9f825c8697a73603f3179f5ee4a378f22a27c
David Ralph <ohlookitsderpy@protonmail.com>
2020-01-02 15:03:26 +0000

    good code

--- index.js
+++ index.js
@@ -4,32 +4,32 @@
  * @author eartharoid (Mue)
  * @license MIT
  */
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client({
     autoReconnect: true,
     disableEveryone: true
 });
-const package = require("./package.json");
-const config = require("./config.js");
-const log = require("leekslazylogger");
+const package = require('./package.json');
+const config = require('./config.js');
+const log = require('leekslazylogger');
 
 log.init({
-    name: "Mue"
+    name: 'Mue'
 });
 log.info(`Mue v${package.version}`, 'magentaBright');
-log.info("Starting up...");
+log.info('Starting up...');
 
 client.on('ready', () => {
-    log.success("Connected to Discord API")
-    log.success(`Logged in as ${client.user.tag}`)
+    log.success('Connected to Discord API');
+    log.success(`Logged in as ${client.user.tag}`);
 });
 
 client.on('guildMemberAdd', (member) => {
-    log.console(`${member.user.tag} has joined`)
+    log.console(`${member.user.tag} has joined`);
     if (member.manageable) {
-        member.addRole(member.addRole(member.guild.roles.find(r => r.name === config.autorole_name).id))
-        .then(log.console(`Giving ${member.user.tag} the ${config.autorole_name} role`))
-        .catch(log.error(`Couldn't give ${member.user.tag} the ${config.autorole_name} role - an error occured.`));
+        member.addRole(member.addRole(member.guild.roles.find(r => r.name === config.autorole_name).id));
+        .then(log.console(`Giving ${member.user.tag} the ${config.autorole_name} role`));
+        .catch(log.error(`Couldn't give ${member.user.tag} the ${config.autorole_name} role - an error occurred.`));
     }
 });
 
